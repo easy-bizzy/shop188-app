a6bec930cb0261910a322575da751b4d60d2be26
easy-bizzy <119699175+easy-bizzy@users.noreply.github.com>
2026-07-16 00:13:02 +0400

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,4 +1,4 @@
 // Ссылка на ваш Google Apps Script
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxfe9mA345oT_em_xK2OrjiY7KxwYIjG5COU5LhqTsAybm6bCUg_GxXDDU_kn6x5Hfv/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzDlIa--ef_TrhkyyBJLxmGKv9KWCRhtV2y7rt9UY2SKDN7JGJpAelijBDAoqqKheOrew/exec';
 
 console.log("Конфигурация синхронизации загружена");
